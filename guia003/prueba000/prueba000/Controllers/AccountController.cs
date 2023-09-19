a88f3f680cb8fecb4b469fbc0c94a10ddac53e9f
Hecalfa <rivaspayes@gmail.com>
2023-09-19 17:54:29 +0200

    avances en la guia 3 y prubas de igbore

--- guia003/prueba000/prueba000/Controllers/AccountController.cs
+++ guia003/prueba000/prueba000/Controllers/AccountController.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AutenticacionCookieControladores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login(string login, string password)
+        {
+            if (login == "admin" && password == "1234")
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, login),
+                };
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var authProperties = new AuthenticationProperties
+                {
+
+                };
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity), authProperties);
+
+                return Ok("Inicio sesión correctamente.");
+            }
+            else
+            {
+                return Unauthorized("Credenciales incorrectas");
+            }
+        }
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Ok("Cerro sesión correctamente.");
+        }
+    }
+}
---
+++ guia003/prueba000/prueba000/Controllers/ProtetedController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutenticacionCookieControladores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProtectedController : ControllerBase
+    {
+        static List<object> data = new List<object>();
+
+        [HttpGet]
+        public IEnumerable<object> Get()
+        {
+            return data;
+        }
+        [HttpPost]
+        public IActionResult Post(string name, string lastName)
+        {
+            data.Add(new { name, lastName });
+            return Ok();
+        }
+    }
+}
