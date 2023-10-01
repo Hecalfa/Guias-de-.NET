7eaa86024de050ee295d89576e46abe92b420263
Hecalfa <rivaspayes@gmail.com>
2023-10-01 16:19:34 +0200

    trabajando en la guia 4

--- guia004/AutenticacionJWTMinimalAPI(prueba000)/AutenticacionJWTMinimalAPI(prueba000)/Auth/JwtAuthenticationService.cs
+++ guia004/AutenticacionJWTMinimalAPI(prueba000)/AutenticacionJWTMinimalAPI(prueba000)/Auth/JwtAuthenticationService.cs
@@ -14,6 +14,21 @@ namespace AutenticacionJWTMinimalAPI_prueba000_
         {
             _key = key;
         }
-        public string Authenticate(s)
+        public string Authenticate(string userName)
+        {
+            var tokenHadler = new JwtSecurityTokenHandler();
+            var tokenKey = Encoding.ASCII.GetBytes(_key);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, userName)
+                }),
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHadler.CreateToken(tokenDescriptor);
+            return tokenHadler.WriteToken(token);
+        }
     }
 }
--- guia004/AutenticacionJWTMinimalAPI(prueba000)/AutenticacionJWTMinimalAPI(prueba000)/Endpoints/AccountEndpoint.cs
+++ guia004/AutenticacionJWTMinimalAPI(prueba000)/AutenticacionJWTMinimalAPI(prueba000)/Endpoints/AccountEndpoint.cs
@@ -1,6 +1,24 @@
-﻿namespace AutenticacionJWTMinimalAPI_prueba000_.Endpoints
+﻿using AutenticacionJWTMinimalAPI_prueba000_.Auth;
+using System.Runtime.CompilerServices;
+
+namespace AutenticacionJWTMinimalAPI_prueba000_.Endpoints
 {
-    public class AccountEndpoint
+    public static class AccountEndpoint
     {
+        public static void AddAccountEndpoints(this WebApplication app)
+        {
+            app.MapPost("/account/login", (string login, string password, IJwtAuthenticationService authService) =>
+            {
+                if(login == "admin" &&  password == "1234")
+                {
+                    var token = authService.Authenticate(login);
+                    return Results.Ok(token);
+                }
+                else
+                {
+                    return Results.Unauthorized();
+                }
+            });
+        }
     }
 }
